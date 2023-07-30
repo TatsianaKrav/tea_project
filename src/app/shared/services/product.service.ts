f9b982ee97ec317c40dbb71c6cd508dbcec45942
Tatiana Kravchenko <tats.krauchanka@gmail.com>
2023-07-30 12:43:44 +0300

    ready project

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -8,15 +8,20 @@ import {ProductType} from "../../../types/product-type";
 })
 export class ProductService {
   productTitle: string = '';
-
   inputNavbarValue: string = '';
 
 
-  constructor(private http:HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
-  getProduct(param: string): Observable<ProductType[]> {
-    return this.http.get<ProductType[]>("https://testologia.site/tea?search=" + param);
+  // getProductId(param: string): string {
+  //   let result = this.http.get<{ id: ProductType }>("https://testologia.site/tea?search=" + param);
+  //   return  Object.keys(result)[0];
+  // }
+
+
+  getProduct(param: string): Observable<{ id: ProductType }> {
+    return this.http.get<{ id: ProductType }>("https://testologia.site/tea?search=" + param);
   }
 
 
--- src/app/views/products/products/products.component.ts
+++ src/app/views/products/products/products.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
-import {Subject, Subscription, tap} from "rxjs";
+import {map, Subject, Subscription, tap} from "rxjs";
 import {ProductType} from "../../../../types/product-type";
 import {ProductsService} from "../../../shared/services/products.service";
 import {ProductService} from "../../../shared/services/product.service";
@@ -13,11 +13,15 @@ import {ProductService} from "../../../shared/services/product.service";
 
 export class ProductsComponent implements OnInit {
 
+  @ViewChild('products')
+  private productsElement!: ElementRef;
+
   products: ProductType[] = [];
   foundProducts: ProductType[] = [];
   loading: boolean = false;
   notFound: boolean = true;
   isDone: boolean = false;
+  idParam: string = '';
 
   subject: Subject<string>;
   productsTitleDefault: string;
@@ -30,9 +34,6 @@ export class ProductsComponent implements OnInit {
     this.productsTitleDefault = this.productsService.productsTitle;
 
     this.subject = new Subject<string>();
-    // this.subject.next(this.productService.inputNavbarValue);
-    this.subject.next(this.value);
-
 
   }
 
@@ -44,79 +45,101 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
 
+    this.loading = true;
+
+    this.productsService.getProducts()
+      .pipe(
+        tap(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe({
+        next: data => {
+          this.products = data;
+        },
+        error: (error) => {
+          console.log(error);
+          this.router.navigate(['/']);
+        }
+      })
+
+
     // this.subscription = this.subject.subscribe((param: string) => {
     //   this.productsService.productsTitle = `Результаты поиска по запросу "${param}"`;
-    //   this.isDone = true;
     //
-    //   // if (param) {
-    //   //   this.isDone = true;
-    //   //   this.productsService.productsTitle = `Результаты поиска по запросу "${param}"`;
+    //
+    //   if (param) {
+    //     this.isDone = true;
+    //     let id: string = '';
     //
     //     this.productService.getProduct(param)
+    //       .pipe(
+    //         tap(item => {
+    //           for (const prop in item) {
+    //             console.log(item[prop]);
+    //           }
+    //         }), //id = Object.keys(item)[0]
+    //       )
     //       .subscribe(
     //         {
-    //           next: data => {
+    //           next: (data) => {
     //             console.log(data);
-    //             this.foundProducts = data;
-    //             console.log(this.foundProducts);
     //           },
     //           error: (error) => {
     //             console.log(error);
-    //             // this.router.navigate(['/']);
     //           }
     //         }
     //       )
-    //   // } else {
-    //   //   this.productsService.productsTitle = this.productsTitleDefault;
-    //   // }
+    //   } else {
+    //     this.productsService.productsTitle = this.productsTitleDefault;
+    //     this.loading = true;
+    //
+    //     this.productsService.getProducts()
+    //       .pipe(
+    //         tap(() => {
+    //           this.loading = false;
+    //         })
+    //       )
+    //       .subscribe({
+    //         next: data => {
+    //           this.products = data;
+    //         },
+    //         error: (error) => {
+    //           console.log(error);
+    //           this.router.navigate(['/']);
+    //         }
+    //       })
+    //   }
+    //
     // })
     //
-    //
-    // if (this.isDone && !this.foundProducts) {
-    //   this.products = [];
-    //   this.notFound = true;
-    // } else if (this.isDone && this.foundProducts) {
-    //   this.products = this.foundProducts;
-    //   console.log(this.products);
-    // } else {
-    //   this.loading = true;
-    //
-    //   this.productsService.getProducts()
-    //     .pipe(
-    //       tap(() => {
-    //         this.loading = false;
-    //       })
-    //     )
-    //     .subscribe({
-    //       next: data => {
-    //         this.products = data;
-    //       },
-    //       error: (error) => {
-    //         console.log(error);
-    //         this.router.navigate(['/']);
-    //       }
-    //     })
-    //
-    // }
-
-    this.loading = true;
-
-      this.productsService.getProducts()
-        .pipe(
-          tap(() => {
-            this.loading = false;
-          })
-        )
-        .subscribe({
-          next: data => {
-            this.products = data;
-          },
-          error: (error) => {
-            console.log(error);
-            this.router.navigate(['/']);
-          }
-        })
-
+    // this.subject.next(this.value);
 
   }
 }
+
+
+// if (this.isDone && Array.isArray(this.foundProducts)) {
+//   this.products = [];
+//   this.notFound = true;
+// } else if (this.isDone && ! Array.isArray(this.foundProducts)) {
+//   this.products = this.foundProducts;
+// } else {
+//   this.loading = true;
+//
+//   this.productsService.getProducts()
+//     .pipe(
+//       tap(() => {
+//         this.loading = false;
+//       })
+//     )
+//     .subscribe({
+//       next: data => {
+//         this.products = data;
+//       },
+//       error: (error) => {
+//         console.log(error);
+//         this.router.navigate(['/']);
+//       }
+//     })
+// }
